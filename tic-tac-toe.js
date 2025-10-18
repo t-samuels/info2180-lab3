dd5ba9dc3bbc0c921e3cd06dbcd4f8e78b611a46
Tiana Samuels <tiana.samuels@gmail.com>
2025-10-17 22:41:43 -0500

    Edited javascript file

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -22,19 +22,19 @@ window.addEventListener("DOMContentLoaded", function() {
 
     function checkWinner() {
         const winningCombos = [
-            [0, 1, 2]
-            [3, 4, 5]
-            [6, 7, 8]
-            [0, 3, 6]
-            [1, 4, 7]
-            [2, 5, 8]
-            [0, 4, 8]
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
             [2, 4, 6]
         ];
 
         for (let combo of winningCombos) {
             const [a, b, c] = combo;
-            if (gameState[a] && gameState[a] === gameState[c]) {
+            if (gameState[a] && gameState[a] === gameState[b] && gameState[c]) {
                 const winner = gameState[a];
                 statusDiv.textContent = `Congratulations! ${winner} is the Winner!`;
                 statusDiv.classList.add("you-won");
