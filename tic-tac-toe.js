ea8a6da08e3d18099be727761721fa28f3b8bfac
Tiana Samuels <tiana.samuels@gmail.com>
2025-10-17 21:16:28 -0500

    Edited javascript file

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -9,3 +9,30 @@ window.addEventListener("DOMContentLoaded", function() {
         square.classList.add("square");
     });
 });
+//selects all the squares in the grid
+const squares = document.querySelectorAll('.square');
+
+//keeps track of whose turn it is
+let currentPlayer = 'X';
+
+//tracks state of game
+let gameState = Array(9).fill(null);
+
+squares.forEach((square, index) => {
+      square.addEventListener('click', () => {
+        if (square.textContent !== '') 
+            return;
+
+        //updates the square content
+        square.textContent = currentPlayer;
+
+        //adds class "X" or "O" for styling
+        square.classList.add(currentPlayer);
+
+        //updates the game state array
+        gameState[index] = currentPlayer;
+
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+      });
+});
+
